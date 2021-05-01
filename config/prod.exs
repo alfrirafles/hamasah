985a993ebe470e71807a9f6378337bdd364e89ed
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-05-01 22:18:41 +0700

    Updated the endpoint to resolve WebSocket handshake issue.

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :odoj, OdojWeb.Endpoint,
-  url: [host: "odoj.gigalixirapp.com", port: 80, scheme: "https"],
+  url: [host: "hamasah.gigalixirapp.com", port: 80, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
